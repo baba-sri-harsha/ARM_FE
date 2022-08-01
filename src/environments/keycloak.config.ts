033fc58836badc483207398008c81d108a499677
Dibya Prakash Ojha <108319815+mr-dibyaprakash@users.noreply.github.com>
2022-08-01 14:04:26 +0530

    Update keycloak.config.ts

--- src/environments/keycloak.config.ts
+++ src/environments/keycloak.config.ts
@@ -1,11 +1,9 @@
 import { KeycloakConfig } from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:7688',
+  url: 'http://192.168.1.39:7688',
   realm: 'myrealm',
   clientId: 'myclient'
 };
 
 export default keycloakConfig;
-
-// url: 'http://192.168.1.39:7688',
